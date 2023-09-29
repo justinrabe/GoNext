71a8b93596a23f1eae24dfbf32bc2d9c0c94bbad
Justin <jtr342@gmail.com>
2023-09-28 21:28:30 -0700

    Aggregated tournaments dataset in CTE

--- backend/queries/tournaments.sql
+++ backend/queries/tournaments.sql
@@ -1,15 +1,25 @@
 
+WITH dataset AS (
+    SELECT 
+        teamers.id AS team_id,
+        teamers.side,
+        teamers.result
+    FROM tournaments, 
+    UNNEST(stages) t (stagers), 
+    UNNEST(stagers.sections) u (sec),
+    UNNEST(sec.matches) m (matchers),
+    UNNEST(matchers.games) g (gamers),
+    UNNEST(gamers.teams) te (teamers)
+    WHERE tournaments.slug = 'msi_2023'
+    
+    LIMIT 10
+)
+SELECT 
+t.name,
+d.side,
+d.result
+FROM 
+dataset d
+JOIN teams t 
+   ON d.team_id = t.team_id 
 
-SELECT tournaments.slug, 
-stagers.name, 
-stagers.slug, 
-sec.name, 
-matchers.id,
-stagers.sections
-FROM tournaments, 
-UNNEST(stages) t (stagers), 
-UNNEST(stagers.sections) u (sec),
-UNNEST(sec.matches) m (matchers)
-
-WHERE tournaments.slug = 'msi_2023'
-LIMIT 10

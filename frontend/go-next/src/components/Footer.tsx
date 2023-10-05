9f30d71c356d3e8248ff702e59031e83c79e59b2
Gabe <28gabe@gmail.com>
2023-10-05 10:36:45 -0700

    adds prettier format config

--- frontend/go-next/src/components/Footer.tsx
+++ frontend/go-next/src/components/Footer.tsx
@@ -1,87 +1,81 @@
 import Button from './Button';
 
 export default function Footer() {
-	return (
-		<footer className='w-full bg-black text-white px-[3.75rem] py-10'>
-			<h1 className='uppercase text-gold4 font-display font-medium'>
-				Ranking Updates
-			</h1>
-			<div className='flex justify-between'>
-				<div>
-					<h1 className='text-2xl italic font-display font-medium uppercase'>
-						Stay Ahead of the Game
-					</h1>
-					<h2>
-						Get exclusive updates on GO NEXT Power Rankings and
-						more.
-					</h2>
-				</div>
-				<div className='flex gap-2'>
-					<input
-						type='text'
-						name='email'
-						id=''
-						placeholder='Email address'
-						className='rounded py-2 px-4 w-[300px] bg-transparent border border-grey'
-					/>
-					<Button>Sign Up</Button>
-				</div>
-			</div>
-			<hr className='my-8 border-grey' />
+  return (
+    <footer className='w-full bg-black px-[3.75rem] py-10 text-white'>
+      <h1 className='font-display font-medium uppercase text-gold4'>
+        Ranking Updates
+      </h1>
+      <div className='flex justify-between'>
+        <div>
+          <h1 className='font-display text-2xl font-medium uppercase italic'>
+            Stay Ahead of the Game
+          </h1>
+          <h2>Get exclusive updates on GO NEXT Power Rankings and more.</h2>
+        </div>
+        <div className='flex gap-2'>
+          <input
+            type='text'
+            name='email'
+            id=''
+            placeholder='Email address'
+            className='w-[300px] rounded border border-grey bg-transparent px-4 py-2'
+          />
+          <Button>Sign Up</Button>
+        </div>
+      </div>
+      <hr className='my-8 border-grey' />
 
-			{/* Link section */}
-			<div className='flex flex-start gap-6'>
-				<div className='flex flex-col w-[17rem]'>
-					<h1 className='font-semibold'>GO NEXT</h1>
-					<a href='#'>Meet the Team</a>
-				</div>
-				<div className='flex flex-col w-[17rem]'>
-					<h1 className='font-semibold'>Products</h1>
-					<a href='#'>LoL Power Rankings</a>
-				</div>
-				<div className='flex flex-col w-[17rem]'>
-					<h1 className='font-semibold'>Resources</h1>
-					<a href='#'>Technical Documentation</a>
-					<a href='#'>Source Code</a>
-				</div>
-			</div>
+      {/* Link section */}
+      <div className='flex-start flex gap-6'>
+        <div className='flex w-[17rem] flex-col'>
+          <h1 className='font-semibold'>GO NEXT</h1>
+          <a href='#'>Meet the Team</a>
+        </div>
+        <div className='flex w-[17rem] flex-col'>
+          <h1 className='font-semibold'>Products</h1>
+          <a href='#'>LoL Power Rankings</a>
+        </div>
+        <div className='flex w-[17rem] flex-col'>
+          <h1 className='font-semibold'>Resources</h1>
+          <a href='#'>Technical Documentation</a>
+          <a href='#'>Source Code</a>
+        </div>
+      </div>
 
-			<hr className='my-8 border-grey' />
+      <hr className='my-8 border-grey' />
 
-			<div className='flex items-center justify-center gap-[100px]'>
-				<a href='#'>
-					<img src='/assets/riot-games-logo.png' width={145} />
-				</a>
-				<a href='#'>
-					<img src='/assets/powered-by-aws-logo.png' width={145} />
-				</a>
-				<a href='#'>
-					<img src='/assets/devpost-logo.png' width={145} />
-				</a>
-			</div>
+      <div className='flex items-center justify-center gap-[100px]'>
+        <a href='#'>
+          <img src='/assets/riot-games-logo.png' width={145} />
+        </a>
+        <a href='#'>
+          <img src='/assets/powered-by-aws-logo.png' width={145} />
+        </a>
+        <a href='#'>
+          <img src='/assets/devpost-logo.png' width={145} />
+        </a>
+      </div>
 
-			<div className='text-grey text-center leading-6 mt-8'>
-				™ & © 2023 Riot Games, Inc. League of Legends and all related
-				logos, characters, and distinctive likenesses thereof are
-				exclusive property of Riot Games, Inc. All Rights Reserved.
-			</div>
+      <div className='mt-8 text-center leading-6 text-grey'>
+        ™ & © 2023 Riot Games, Inc. League of Legends and all related logos,
+        characters, and distinctive likenesses thereof are exclusive property of
+        Riot Games, Inc. All Rights Reserved.
+      </div>
 
-			<div className='flex w-full justify-center items-center mt-8'>
-				<div className='flex bg-[#2E456C80] p-2 rounded gap-2'>
-					<img
-						src='/assets/esrb.png'
-						className='w-[100px] h-[150px]'
-					/>
-					<div className='flex flex-col leading-6'>
-						<div>Blood</div>
-						<div>Fantasy</div>
-						<div>Mild Suggestive Themes</div>
-						<div>Use of Alcohol and Tobacco</div>
-						<hr className='my-4 border-grey' />
-						<div>Online Interactions Not Rated by the ESRB</div>
-					</div>
-				</div>
-			</div>
-		</footer>
-	);
+      <div className='mt-8 flex w-full items-center justify-center'>
+        <div className='flex gap-2 rounded bg-[#2E456C80] p-2'>
+          <img src='/assets/esrb.png' className='h-[150px] w-[100px]' />
+          <div className='flex flex-col leading-6'>
+            <div>Blood</div>
+            <div>Fantasy</div>
+            <div>Mild Suggestive Themes</div>
+            <div>Use of Alcohol and Tobacco</div>
+            <hr className='my-4 border-grey' />
+            <div>Online Interactions Not Rated by the ESRB</div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- frontend/go-next/src/components/Navbar.tsx
+++ frontend/go-next/src/components/Navbar.tsx
@@ -1,30 +1,27 @@
 export default function Navbar() {
-	return (
-		<>
-			<header className='bg-black w-full px-[60px] py-[24px] flex items-center'>
-				<a href='#' className='pr-[40px] border-r border-grey '>
-					<img src='/assets/league-logo.png' className='w-[150px]' />
-				</a>
-				<img
-					src='/assets/go-next-logo.png'
-					className='w-[160px] ml-[40px]'
-				/>
-				<nav className='ml-[40px]'>
-					<ul className='flex text-white gap-x-10'>
-						<NavLink>Home</NavLink>
-						<NavLink>Rankings</NavLink>
-						<NavLink>Documentation</NavLink>
-					</ul>
-				</nav>
-			</header>
-		</>
-	);
+  return (
+    <>
+      <header className='flex w-full items-center bg-black px-[60px] py-[24px]'>
+        <a href='#' className='border-r border-grey pr-[40px] '>
+          <img src='/assets/league-logo.png' className='w-[150px]' />
+        </a>
+        <img src='/assets/go-next-logo.png' className='ml-[40px] w-[160px]' />
+        <nav className='ml-[40px]'>
+          <ul className='flex gap-x-10 text-white'>
+            <NavLink>Home</NavLink>
+            <NavLink>Rankings</NavLink>
+            <NavLink>Documentation</NavLink>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 }
 
 function NavLink({ children }) {
-	return (
-		<li className='hover:cursor-pointer'>
-			<a href='/'>{children}</a>
-		</li>
-	);
+  return (
+    <li className='hover:cursor-pointer'>
+      <a href='/'>{children}</a>
+    </li>
+  );
 }

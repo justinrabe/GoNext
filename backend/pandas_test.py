b78bc24936e1809b136c9e8925ed577b5214c7bf
Justin <jtr342@gmail.com>
2023-10-04 20:58:47 -0700

    converted output to list of dictionaries

--- backend/pandas_test.py
+++ backend/pandas_test.py
@@ -2,13 +2,13 @@ import pandas as pd
 import json
 import team
 
-teams_in_tournament = {}
+teams_in_tournament = []
 
 stages_k = {
 "Groups" : 16,
 "knockouts" : 8, 
-"Regular Season" : 16, 
-"Playoffs" : 32, 
+"Regular Season" : 32, 
+"Playoffs" : 100, 
 "Regional Finals" : 32, 
 "Round 1" : 4, 
 "Round 2" : 8, 
@@ -31,27 +31,14 @@ with open("esports-data/teams.json", "r") as json_file:
 def tournament_rankings(tournament):
     with open("esports-data/tournaments.json", "r") as json_file:
         tournaments_data = json.load(json_file)
-    # df = pd.json_normalize(tournaments_data,  meta=[ ['stages', 'sections', 'matches', 'teams' 'id']])
-    # df = df.rename(columns={
-    #     'stages.sections.matches.teams.id' : 'teamID'
-    # })
-    # print(list(df.columns))
-    # print(df)
+
     to_df = pd.DataFrame(tournaments_data)
     filtered_tournament = to_df[to_df["slug"] == tournament]
     ##print(filtered_tournament)
     for stage in filtered_tournament["stages"]:
         stage = pd.DataFrame(stage)
-        # print(stage["name"])
-        # print()
-        # print(stage["name"][0])
-        # k = stages_k["Regular Season"]
-        # print(k)
         for section in stage["sections"]:
-            
-            # print(type(section))
             section = pd.DataFrame(section)
-            # print(section)
             for match in section["matches"]:
                 match = pd.DataFrame(match)
                 for game in match["games"]:
@@ -62,39 +49,57 @@ def tournament_rankings(tournament):
                     for team in game["teams"]:
                         blue_team = team[0]
                         red_team = team[1]
-                        
-                        ##team creation
-                        blue_team_name = get_team(blue_team["id"])["name"]
-                        if blue_team_name not in teams_in_tournament:
-                            teams_in_tournament[blue_team_name] = 1000
+                        b = get_team(blue_team["id"])
+                        blue_team_name = b["name"]
+                        if not any (d['team_name'] == b["name"] for d in teams_in_tournament):
+                            teams_in_tournament.append({
+                                "id" : b["team_id"],
+                                "team_name" : b["name"],
+                                "rank" : 0,
+                                "region" : None,
+                                "elo" : 1000
+                            })
 
-                        red_team_name = get_team(red_team["id"])["name"]
-                        if red_team_name not in teams_in_tournament:
-                            teams_in_tournament[red_team_name] = 1000
+                        
+                        r = get_team(red_team["id"])
+                        red_team_name = r["name"]
+                        if not any (d['team_name'] == r["name"] for d in teams_in_tournament):
+                            teams_in_tournament.append({
+                                "id" : r["team_id"],
+                                "team_name" : r["name"],
+                                "rank" : 0,
+                                "region" : None,
+                                "elo" : 1000
+                            })
 
                         ## Blue Team wins
                         if (team[0]["result"]["outcome"] == "win"):
-                            blue_new_elo = calculate_elo_rating(teams_in_tournament[blue_team_name], teams_in_tournament[red_team_name], 1, k)
-                            red_new_elo = calculate_elo_rating(teams_in_tournament[red_team_name], teams_in_tournament[blue_team_name], 0, k)
-                            teams_in_tournament[blue_team_name] = blue_new_elo
-                            teams_in_tournament[red_team_name] = red_new_elo
-                        ## Red Team wins
-                        elif (team[0]["result"]["outcome"] == "lose"):
-                            blue_new_elo = calculate_elo_rating(teams_in_tournament[blue_team_name], teams_in_tournament[red_team_name], 0, k)
-                            red_new_elo = calculate_elo_rating(teams_in_tournament[red_team_name], teams_in_tournament[blue_team_name], 1, k)
-                            teams_in_tournament[blue_team_name] = blue_new_elo
-                            teams_in_tournament[red_team_name] = red_new_elo
+                            # res = next(t for t in teams_in_tournament if t["team_name"] == blue_team_name)["elo"]
+                            # print (res)
+                            blue_new_elo = calculate_elo_rating(
+                                next(t for t in teams_in_tournament if t["team_name"] == blue_team_name)["elo"],
+                                next(t for t in teams_in_tournament if t["team_name"] == red_team_name)["elo"], 1, k)
+                            red_new_elo = calculate_elo_rating(
+                                next(t for t in teams_in_tournament if t["team_name"] == red_team_name)["elo"],
+                                next(t for t in teams_in_tournament if t["team_name"] == blue_team_name)["elo"], 0, k)
+                            # teams_in_tournament[blue_team_name] = blue_new_elo
+                            for t in teams_in_tournament:
+                                if t["team_name"] == blue_team_name:
+                                    t["elo"] = blue_new_elo
+                                elif t["team_name"] == red_team_name:
+                                    t["elo"] = red_new_elo
 
-                            ##elo rating formula
-                            ##save elo to elo data structure
-                            ##to-do create elo data structure from all teams
-    # [print(i) for i in teams_in_tournament]
-    print (teams_in_tournament)
+    sorted_teams = sorted(teams_in_tournament, key=lambda x: x['elo'], reverse=True)
+    for index, team_data in enumerate(sorted_teams, start=1):
+        team_data['rank'] = index
+    to_json = json.dumps(sorted_teams)
+    print(to_json)
 
 def get_team(id):
     output = [t for t in teams_data if t['team_id'] == str(id)]
     return output[0]
-                         
+    
+  
 def calculate_elo_rating(player_rating, opponent_rating, outcome, k=32):
     """
     Calculate the new Elo rating for a player.
@@ -110,4 +115,4 @@ def calculate_elo_rating(player_rating, opponent_rating, outcome, k=32):
     return new_rating
 
 
-tournament_rankings("lck_spring_2023")
+tournament_rankings("lcs_spring_2023")

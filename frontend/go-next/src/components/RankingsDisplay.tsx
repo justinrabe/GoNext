daca97919acfe46eee89cc83d8ba90477f3e29a6
Gabe <28gabe@gmail.com>
2023-10-18 21:50:06 -0700

    fixes type errors affecting build

--- frontend/go-next/src/components/RankingsDisplay.tsx
+++ frontend/go-next/src/components/RankingsDisplay.tsx
@@ -11,79 +11,6 @@ import { Team, Tournament } from '../types';
 import s3Client from '../utils/s3';
 import { ListObjectsV2Command, GetObjectCommand } from '@aws-sdk/client-s3';
 
-const sampleData: Team[] = [
-  {
-    id: '98767991877340524',
-    team_name: 'Cloud9',
-    rank: 1,
-    region: null,
-    elo: 1163.7735983990156,
-  },
-  {
-    id: '99294153824386385',
-    team_name: 'Golden Guardians',
-    rank: 2,
-    region: null,
-    elo: 1062.5207472791712,
-  },
-  {
-    id: '98926509892121852',
-    team_name: 'FlyQuest',
-    rank: 3,
-    region: null,
-    elo: 1059.08254353179,
-  },
-  {
-    id: '98767991860392497',
-    team_name: 'TSM',
-    rank: 4,
-    region: null,
-    elo: 1009.9364753632694,
-  },
-  {
-    id: '98926509884398584',
-    team_name: 'CLG',
-    rank: 5,
-    region: null,
-    elo: 993.7235565323087,
-  },
-  {
-    id: '98926509885559666',
-    team_name: 'Team Liquid Honda',
-    rank: 6,
-    region: null,
-    elo: 963.6182234370709,
-  },
-  {
-    id: '103461966951059521',
-    team_name: 'Evil Geniuses LG',
-    rank: 7,
-    region: null,
-    elo: 957.103116751816,
-  },
-  {
-    id: '99294153828264740',
-    team_name: '100 Thieves',
-    rank: 8,
-    region: null,
-    elo: 941.0037538296394,
-  },
-  {
-    id: '98767991930907107',
-    team_name: 'Immortals Progressive',
-    rank: 9,
-    region: null,
-    elo: 931.0342522216347,
-  },
-  {
-    id: '98926509883054987',
-    team_name: 'Dignitas',
-    rank: 10,
-    region: null,
-    elo: 918.2037326542838,
-  },
-];
-
 const tournamentOptions: Tournament[] = [
   {
     name: 'LPL Spring 2023',
@@ -145,7 +72,7 @@ export default function RankingsDisplay() {
   fetchTournamentList();
 
   // Helper function to convert a web-streams-polyfill ReadableStream to a string
-  const streamToString = async (stream) => {
+  const streamToString = async (stream: ReadableStream) => {
     const reader = stream.getReader();
     let result = '';
     while (true) {
@@ -167,7 +94,7 @@ export default function RankingsDisplay() {
     try {
       const data = await s3Client.send(command);
       // Convert the readable stream to text
-      const objectData = await streamToString(data.Body);
+      const objectData = await streamToString(data.Body as ReadableStream);
       // Now parse the text as JSON
       const jsonData = JSON.parse(objectData);
       console.log(jsonData);

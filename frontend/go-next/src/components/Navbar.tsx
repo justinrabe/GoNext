3f7ba13abf965cf6cdaa66c329961d745579af56
Gabe <28gabe@gmail.com>
2023-10-22 23:49:22 -0700

    light responsiveness

--- frontend/go-next/src/components/Navbar.tsx
+++ frontend/go-next/src/components/Navbar.tsx
@@ -3,13 +3,13 @@ import { ReactNode } from 'react';
 export default function Navbar() {
   return (
     <>
-      <header className='flex w-full items-center bg-black px-[60px] py-[24px]'>
+      <header className='flex w-full flex-col items-center bg-black px-[60px] py-[24px] md:flex-row'>
         <a href='#' className='border-r border-grey pr-[40px] '>
           <img src='/assets/league-logo.png' className='w-[150px]' />
         </a>
         <img src='/assets/go-next-logo.png' className='ml-[40px] w-[160px]' />
         <nav className='ml-[40px]'>
-          <ul className='flex gap-x-10 text-white'>
+          <ul className='flex flex-col gap-x-10 text-white md:flex-row'>
             <NavLink>Home</NavLink>
             <NavLink>About Us</NavLink>
             <NavLink>Documentation</NavLink>
